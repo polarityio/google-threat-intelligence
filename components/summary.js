b1549f7aad6fe020d07a6953c7cbf52f372c24d2
Raluca Rusu <rrusu@threatconnect.com>
2025-11-25 08:43:11 +0200

    INT-1489: Google Threat Intel update (#8)
    * chore: Keep setup-related files up-to-date.
    
    * chore: Remove config.js file and its references.
    
    * Remove the config options for custom Baseline Investigation Threshold, and always use defaults from GTI.
    
    * Update graphics for displaying Threat Score.
    
    * UI improvements.
    
    * Highlight Verdict instead of Severity in summary tag.
    
    * Improve font sizes on Score Graphic.
    
    * Update the README file.
    
    * Cleaning up the code.

--- components/summary.js
+++ components/summary.js
@@ -5,24 +5,8 @@
 'use strict';
 polarity.export = PolarityComponent.extend({
   details: Ember.computed.alias('block.data.details'),
+  verdict: Ember.computed.alias('details.verdict'),
+  threatScore: Ember.computed.alias('details.gtiAssessment.threat_score.value'),
   threatsCount: Ember.computed.alias('details.threatsCount'),
   reportsCount: Ember.computed.alias('details.reportsCount'),
-  baselineInvestigationThreshold: Ember.computed(
-    'block.data.details.positives',
-    function () {
-      const rules = this.get('details.compiledBaselineInvestigationRules');
-
-      const positives = this.get('details.positives');
-      for (let i = 0; i < rules.length; i++) {
-        let rule = rules[i];
-        if (positives >= rule.min && positives <= rule.max) {
-          return {
-            message: rule.message,
-            level: rule.level
-          };
-        }
-      }
-      return null;
-    }
-  )
 });
